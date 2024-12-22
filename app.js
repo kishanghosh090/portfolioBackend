7c4f14dacf4b742f28182a2bcb96c06fd76a030f
KISHAN RANA GHOSH <129781766+kishanghosh090@users.noreply.github.com>
2024-12-22 23:27:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const corsOptions = {
-  origin: "https://react-portfolio-zeta-mocha.vercel.app",
+  origin: "https://react-portfolio-zeta-mocha.vercel.app/",
   optionsSuccessStatus: 200,
 };
 
